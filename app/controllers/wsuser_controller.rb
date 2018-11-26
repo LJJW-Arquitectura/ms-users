2731f89cff0a5ddd6560313fb5538de03a7d944f
Jmad12 <Juanenero@hotmail.com>
2018-11-26 17:37:15 -0500

    Soap !

--- app/controllers/wsuser_controller.rb
+++ app/controllers/wsuser_controller.rb
@@ -2,9 +2,9 @@ class WsuserController < ApplicationController
   soap_service namespace: 'urn::WashOutUser', camelize_wsdl: :lower
   soap_action "soapFind",
               :args   => { :id => :integer},
-              :return => :user
+              :return => {:user => [{:username => :string, :email => :string}]}
   def soapFind
-    @user = Users.find(params[:id])
-    render :soap => @user
+    @user = User.find(params[:id])
+    render :soap => {:user => [{:username => @user.username, :email => @user.email}]}
   end
 end
